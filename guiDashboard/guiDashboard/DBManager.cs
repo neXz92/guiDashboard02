d00c82a3d71a90d7b72b7abebfd19d83ffd1bfd2
nexz92 <Kevin-Dietzen@web.de>
2017-11-29 00:23:10 +0100

    Version 2 mit SQLite

--- guiDashboard/guiDashboard/DBManager.cs
+++ guiDashboard/guiDashboard/DBManager.cs
@@ -3,55 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Data.SQLite;
-using Finisar.SQLite;
-
+using System.Data.SQLite;
 
 namespace sqLite
 {
+    class Person
+    {
+        public string vname;
+        public string nname;
+        public int alter;
+    }
+
     class DBManager
     {
-        //der folgende Code hat in einer Konsolenanwendung wunderbar geklappt, hier in einer .cs datei - im WPF Projekt, erkennt er die using-direktive (oben) schon nicht.
-        //static void Main(string[] args)
-        //{
-
-        //}
-
-        public static SQLiteConnection sqlite_conn;
-        public static SQLiteCommand sqlite_cmd;
-        public static SQLiteDataReader sqlite_datareader;
+        //outdated
 
         public static void testoutput()
         {
-            sqlite_conn = new SQLiteConnection(@"Data Source=C:\Users\Kevin\Desktop\consumption_monitoring.db;Version=3;New=True;Compress=True;");
+            SQLiteConnection conn = new SQLiteConnection(@"Data Source=Data Source=C:\Users\Kevin\Desktop\consumption_monitoring.db;Version=3;New=True;Compress=True;");
+            conn.Open();
 
-            sqlite_conn.Open();
+            var command = conn.CreateCommand();
 
-            var sqlite_cmd = sqlite_conn.CreateCommand();
+            command.CommandText = @"Select * from Track";
+            SQLiteDataReader sdr = command.ExecuteReader();
 
-            //Daten sind schon in File nach "import"
-            sqlite_cmd.CommandText = "CREATE TABLE Track (id integer primary key autoincrement, startpoint text not null, destination text NOT NULL, durationinmin int NOT NULL, trackdate text NOT NULL, starttime text NOT NULL, endtime text NOT NULL);";
-
-            sqlite_cmd.ExecuteNonQuery();
-
-            //Git test von lappi
-            sqlite_cmd.CommandText = @"Select * from Track";
-
-            sqlite_cmd.ExecuteNonQuery();
-
-            SQLiteDataReader sqlite_datareader = sqlite_cmd.ExecuteReader();
-
-            while (sqlite_datareader.Read())
+            while (sdr.Read())
             {
-                Console.WriteLine(sqlite_datareader.GetString(1) + " " + sqlite_datareader.GetString(2));
+                Console.WriteLine(sdr.GetString(1) + " " + sdr.GetString(2));
             }
-            sqlite_datareader.Close();
+            sdr.Close();
 
-            sqlite_conn.Close();
-
+            conn.Close();
 
             Console.WriteLine("Hello");
-            //Console.ReadKey();
         }
     }
 }
--- guiDashboard/guiDashboard/MainWindow.xaml.cs
+++ guiDashboard/guiDashboard/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using sqLite;
-using Finisar.SQLite;
+using System.Data.SQLite;
 
 namespace guiDashboard
 {
@@ -26,49 +26,12 @@ namespace guiDashboard
         {
             InitializeComponent();
 
-           // DBManager.testoutput();
+            //DBManager.testoutput();
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            SQLiteConnection sqlite_conn;
-            SQLiteCommand sqlite_cmd;
-            SQLiteDataReader sqlite_datareader;
-
-            // create a new database connection:
-            sqlite_conn = new SQLiteConnection(@"Data Source=C:\Users\Kevin\Desktop\consumption_monitoring.db;Version=3;New=True;Compress=True;");
-
-            // open the connection:
-            sqlite_conn.Open();
-
-            // create a new SQL command:
-            sqlite_cmd = sqlite_conn.CreateCommand();
-
-            // Let the SQLiteCommand object know our SQL-Query:
-            sqlite_cmd.CommandText = "CREATE TABLE Track (id integer primary key autoincrement, startpoint text not null, destination text NOT NULL, durationinmin int NOT NULL, trackdate text NOT NULL, starttime text NOT NULL, endtime text NOT NULL);";
-            sqlite_cmd.CommandText = @"Select * from Track";
-
-            // Now lets execute the SQL ;D
-            sqlite_cmd.ExecuteNonQuery();
-
-            // Lets insert something into our new table:
-            //sqlite_cmd.CommandText = "INSERT INTO test (id, text) VALUES (1, 'Test Text 1');";
-
-            // And execute this again ;D
-            //sqlite_cmd.ExecuteNonQuery();
-
-            // Now the SQLiteCommand object can give us a DataReader-Object:
-            sqlite_datareader = sqlite_cmd.ExecuteReader();
-
-            // The SQLiteDataReader allows us to run through the result lines:
-            while (sqlite_datareader.Read()) // Read() returns true if there is still a result line to read
-            {
-                // Print out the content of the text field:
-                MessageBox.Show(sqlite_datareader["id"].ToString());
-            }
-
-            // We are ready, now lets cleanup and close our connection:
-            sqlite_conn.Close();
+            MessageBox.Show("Hello World");
         }
     }
 }
