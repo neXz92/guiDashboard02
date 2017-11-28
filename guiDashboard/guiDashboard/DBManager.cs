04c0634dc3ad73216aef04bf5a20d3c8884da925
nexz92 <Kevin-Dietzen@web.de>
2017-11-28 22:40:12 +0100

    rumgeteste mit SQLite..

--- guiDashboard/guiDashboard/DBManager.cs
+++ guiDashboard/guiDashboard/DBManager.cs
@@ -3,50 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
+//using System.Data.SQLite;
+using Finisar.SQLite;
 
-namespace sqLiteTest
+
+namespace sqLite
 {
     class DBManager
     {
         //der folgende Code hat in einer Konsolenanwendung wunderbar geklappt, hier in einer .cs datei - im WPF Projekt, erkennt er die using-direktive (oben) schon nicht.
-        static void Main(string[] args)
-        {
-            SQLiteConnection conn = new SQLiteConnection(@"Data Source=C:\Users\Kevin\Desktop\WS 17-18\GUI\GUI Projekt Doks\Datenbank\consumption_monitoring.db");
-            conn.Open();
+        //static void Main(string[] args)
+        //{
 
-            var command = conn.CreateCommand();
+        //}
+
+        public static SQLiteConnection sqlite_conn;
+        public static SQLiteCommand sqlite_cmd;
+        public static SQLiteDataReader sqlite_datareader;
+
+        public static void testoutput()
+        {
+            sqlite_conn = new SQLiteConnection(@"Data Source=C:\Users\Kevin\Desktop\consumption_monitoring.db;Version=3;New=True;Compress=True;");
+
+            sqlite_conn.Open();
+
+            var sqlite_cmd = sqlite_conn.CreateCommand();
 
             //Daten sind schon in File nach "import"
-            //command.CommandText = @"CREATE TABLE Track (
-            //                        id integer primary key autoincrement,
-            //                        startpoint text not null,
-            //                        destination text NOT NULL,
-            //                        durationinmin int NOT NULL,
-            //                        trackdate text NOT NULL,
-            //                        starttime text NOT NULL,
-            //                        endtime text NOT NULL
-            //                    );";
-            //command.ExecuteNonQuery();
+            sqlite_cmd.CommandText = "CREATE TABLE Track (id integer primary key autoincrement, startpoint text not null, destination text NOT NULL, durationinmin int NOT NULL, trackdate text NOT NULL, starttime text NOT NULL, endtime text NOT NULL);";
 
-            //command.CommandText = @"INSERT INTO Track(id,startpoint,destination,durationinmin,trackdate,starttime,endtime) VALUES (1,'Zweibrücken, Amerikastraße 1','Kaiserslautern, Kammgarn',55,'13.11.2017','09:06','10:01');";
-            //command.ExecuteNonQuery();
+            sqlite_cmd.ExecuteNonQuery();
 
             //Git test von lappi
+            sqlite_cmd.CommandText = @"Select * from Track";
 
-            command.CommandText = @"Select * from Track";
-            SQLiteDataReader sdr = command.ExecuteReader();
+            sqlite_cmd.ExecuteNonQuery();
 
-            while (sdr.Read())
+            SQLiteDataReader sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+            while (sqlite_datareader.Read())
             {
-                Console.WriteLine(sdr.GetString(1) + " " + sdr.GetString(2));
+                Console.WriteLine(sqlite_datareader.GetString(1) + " " + sqlite_datareader.GetString(2));
             }
-            sdr.Close();
+            sqlite_datareader.Close();
 
-            conn.Close();
+            sqlite_conn.Close();
+
 
             Console.WriteLine("Hello");
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
